4654f948f9174217adeaf87ef48c1e7dab0bb212
4a <4a@pianodue15>
2022-04-20 09:48:13 +0200

    ver 2

--- src/MainTest.java
+++ src/MainTest.java
@@ -2,7 +2,8 @@
 public class MainTest {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		FattorialeClass f=new FattorialeClass();
+		System.out.println(f.fattoriale(10));
 
 	}
 
